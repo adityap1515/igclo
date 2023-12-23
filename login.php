b8a07c898fc1cc278e1310b27f2c8720853d97a5
adityap1515 <aditya.ps1515@gmail.com>
2023-12-23 12:54:02 +0530

    Made changes in index

--- login.php
+++ login.php
@@ -16,18 +16,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  // SQL query to check if the user exists
-  $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+ $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
   $result = $conn->query($sql);
 
   if ($result->num_rows > 0) {
-    // User exists and credentials are correct
-    // Perform actions upon successful login, e.g., set session variables or redirect
-    // For demonstration, let's redirect to a welcome page
-    header("Location: welcome.php");
+    header("Location: https://instagram.com");
     exit();
   } else {
-    // Invalid credentials
+
     echo "Invalid username or password";
   }
 }
